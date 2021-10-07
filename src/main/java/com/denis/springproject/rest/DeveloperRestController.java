144ca27a9e400c3dbbbae16a4451b774c0bd3e8f
DenysBorozenets <borozenetsdenysforme@gmail.com>
2021-10-07 13:42:03 +0300

    Add simple spring security, with role and permission

---
+++ src/main/java/com/denis/springproject/rest/DeveloperRestController.java
@@ -0,0 +1,44 @@
+package com.denis.springproject.rest;
+
+import com.denis.springproject.model.Developer;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@RestController
+@RequestMapping("/api/developers")
+public class DeveloperRestController {
+
+    private List<Developer> DEVELOPERS = Stream.of(
+            new Developer(1L, "Ivan", "Ivanov"),
+            new Developer(2L, "Sergey", "Sergeev"),
+            new Developer(3L, "Petr", "Petrow")
+    ).collect(Collectors.toList());
+
+    @GetMapping
+    public List<Developer> getAll() {
+        return DEVELOPERS;
+    }
+
+    @GetMapping("/{id}")
+    public Developer getById(@PathVariable Long id) {
+        return DEVELOPERS.stream().filter(developer -> developer.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+
+    @PostMapping
+    public Developer create(@RequestBody Developer developer) {
+        this.DEVELOPERS.add(developer);
+        return  developer;
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable Long id) {
+        this.DEVELOPERS.removeIf(developer -> developer.getId().equals(id));
+    }
+
+
+}
