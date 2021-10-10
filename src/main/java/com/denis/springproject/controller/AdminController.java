2a93d13b7139c0da5e5951afeb90c24e5830a581
DenysBorozenets <borozenetsdenysforme@gmail.com>
2021-10-10 14:34:08 +0300

    add controllers for update and remove patients

--- src/main/java/com/denis/springproject/controller/AdminController.java
+++ src/main/java/com/denis/springproject/controller/AdminController.java
@@ -36,9 +36,9 @@ public class AdminController {
     }
 
     @PostMapping("/admin/patient/add")
-    public String addPatientPost(@RequestParam String email,
-                                 @RequestParam String firstName,
-                                 @RequestParam String lastName) {
+    public String addPatient(@RequestParam String email,
+                             @RequestParam String firstName,
+                             @RequestParam String lastName) {
         Patient patient = new Patient(email, firstName, lastName);
         patientRepository.save(patient);
         return "redirect:/welcome/admin/patient";
@@ -54,6 +54,38 @@ public class AdminController {
         patient.ifPresent(result::add);
         model.addAttribute("patient", result);
         return "patient-details";
+    }
+
+    @GetMapping("/admin/patient/{id}/edit")
+    public String patientEdit(@PathVariable(value = "id") long id, Model model) {
+        if (!patientRepository.existsById(id)) {
+            return "redirect:/admin/patient";
+        }
+        Optional<Patient> patient = patientRepository.findById(id);
+        ArrayList<Patient> result = new ArrayList<>();
+        patient.ifPresent(result::add);
+        model.addAttribute("patient", result);
+        return "patient-edit";
+    }
+
+    @PostMapping("/admin/patient/{id}/edit")
+    public String patientUpdate(@PathVariable(name = "id") long id,
+                                @RequestParam String email,
+                                @RequestParam String firstName,
+                                @RequestParam String lastName) {
+        Patient patient = patientRepository.findById(id).orElseThrow();
+        patient.setEmail(email);
+        patient.setFirstName(firstName);
+        patient.setLastName(lastName);
+        patientRepository.save(patient);
+        return "redirect:/welcome/admin/patient";
+    }
+
+    @PostMapping("/admin/patient/{id}/remove")
+    public String patientRemove(@PathVariable(name = "id") long id){
+        Patient patient = patientRepository.findById(id).orElseThrow();
+        patientRepository.delete(patient);
+        return "redirect:/welcome/admin/patient";
     }
 
 }
